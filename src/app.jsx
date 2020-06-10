6bbec3bacb4c6138b81f6def18539bfa3768cf4a
冉娃娃 <274544338@qq.com>
2020-06-10 08:12:32 +0800

    feat(src): react组件props
    1. 直接在组件标签上写属性可以向子组件传值
    2. 不能在子组件修改props值
    3. 在jsx中使用注释时,外面必须加上大括号,因为所有js语法都需要加大括号

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-export default class App extends React.Component{
+import React from "react";
+import Home from "./home";
+export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello React</h1>
         <h2>学习,学习,努力学习.</h2>
+        <Home title="传入的标题" appendix="后缀" />
       </div>
-    )
+    );
   }
 }
--- src/home.jsx
+++ src/home.jsx
@@ -4,9 +4,26 @@
  * @author 冉娃娃 <274544338@qq.com>
  * @since 2020/6/8 8:43
  */
-import React from 'react';
+import React from "react";
 export default class Home extends React.Component {
   render() {
-    return <div>这里子子组件</div>
+    {
+      /* 注释外面必须加花括号,因为js语法必须被花括号括起来 */
+    }
+    console.log(this.props); // 所有传入的属性
+    // 子组件里面无法修改props的值
+    return (
+      <div>
+        <div>
+          {this.props.title.split("").map((ele) => (
+            <div key={ele}>
+              {ele}
+              -----
+              {this.props.appendix}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
