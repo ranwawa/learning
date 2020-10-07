a409a24b39c943b3d3532f9bc4fa6c59c31a0cc4
ranwawa <274544338>
2020-10-07 22:12:54 +0800

    feat(高阶路由):

--- src/RouterDemo/WithRouter.jsx
+++ src/RouterDemo/WithRouter.jsx
@@ -1,33 +1,44 @@
 /*
-* 为什么需要withRouter
-*   如果当前组件没有放到<Route>组件里面的话,就无法获取路由相关对象
-*   用这个函数包裹一下即可
-* */
-
+ * 为什么需要withRouter
+ *   如果当前组件没有放到<Route>组件里面的话,就无法获取路由相关对象
+ *   用这个函数包裹一下即可
+ *  为什么函数式组件无法使用withRouter????
+ *  为什么第14行绑定this,无法起作用?
+ * */
 import React from 'react';
 import { withRouter, BrowserRouter, Route } from 'react-router-dom';
-import RouterDemo from './RouterDemo';
 
-function w (props) {
-  console.log('props', props);
-  return (
-    <div>
-      withrouterrrrrrrrrrrrr
-    </div>
-  )
-}
-function c(props) {
-  function handleClick() {
-    console.log('c', props);
+class Child extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
   }
-  return(
-    <BrowserRouter>
-      <Route path="/home" component={ w } />
-      <Route>
-        <button onClick={ handleClick } >点击按钮</button>
-      </Route>
-    </BrowserRouter>
-  )
+  handleClick() {
+    console.log('c', this.props);
+  }
+  render() {
+    return (
+      <div>
+        withrouterrrrrrrrrrrrr
+        <button onClick={this.handleClick.bind(this)}>点击按钮</button>
+      </div>
+    );
+  }
 }
 
-export default c;
+const ChildWithRouter = withRouter(Child);
+function Parent(props) {
+  return (
+    <BrowserRouter>
+      <Route
+        path="/home"
+        component={Child}
+      />
+      {/* 这个组件内部无法获取路由相关信息 */}
+      <Child />
+      <ChildWithRouter />
+    </BrowserRouter>
+  );
+}
+export default Parent;
+
