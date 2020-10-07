060ee37c6ec6bbdd9198c555390dacb3df4351f6
冉娃娃 <274544338@qq.com>
2020-10-07 11:49:36 +0800

    feat(hashRouter):

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'antd/dist/antd.css';
-import RouterDemo from './RouterDemo/RouterDemo';
+import HashRouter from './RouterDemo/HashRouter';
+// import RouterDemo from './RouterDemo/RouterDemo';
 // import { AntdDemo } from './AntdDemo';
 // import PropTypes from './PropTypes';
 // import Compose from './Compose';
@@ -27,7 +28,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <RouterDemo />
+        <HashRouter />
+        {/*<RouterDemo />*/}
         {/*<AntdDemo />*/}
         {/*<PropTypes userName="你是认证" age={ 23 } sex="女"/>*/}
         {/*<Compose left={ <Calculator /> }>*/}
