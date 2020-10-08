1241115d8d5b810bddc26756c36aa220899d39da
ranwawa <274544338>
2020-10-08 20:55:45 +0800

    feat(redux初体验):

--- src/ReduxDemo/Store.jsx
+++ src/ReduxDemo/Store.jsx
@@ -9,30 +9,30 @@
 * dispatch触发的顺序
 *   - subscribe函数
 *   - reducer函数
+*  为什么在组件里面重新支行render无法重新渲染视力呢???
 * */
 
 import React from 'react';
-import { createStore } from 'redux';
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
+// import { createStore } from 'redux';
+// const INCREMENT = 'increment';
+// const DECREMENT = 'decrement';
+// const reducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return state + 1;
+//     case DECREMENT:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+//
+// const store = createStore(reducer);
 
 function ShowValue(props) {
   console.log(props);
   return (
     <div>
-      store的值是 { props.value }
     </div>
   )
 }
@@ -40,17 +40,20 @@ function ShowValue(props) {
 export default class Store extends React.Component{
   constructor() {
     super();
-    store.subscribe(this.render);
+    // store.subscribe(() => this.render)
   }
-  handleClick(type) {
-    store.dispatch({ type });
-  }
+  // handleClick(type) {
+  //   store.dispatch({ type });
+  // }
   render() {
     return (
       <div>
-        <ShowValue value={ store.getState() } />
-        <button onClick={ () => this.handleClick(INCREMENT) }>加1</button>
-        <button onClick={ () => this.handleClick(DECREMENT) }>减1</button>
+        {/*<ShowValue value={ store.getState() } />*/}
+        {/*<button onClick={ () => this.handleClick(INCREMENT) }>加1</button>*/}
+        {/*<button onClick={ () => this.handleClick(DECREMENT) }>减1</button>*/}
+        store的值是 { this.props.value }
+        <button onClick={ this.props.clickIncrement }>加1</button>
+        <button onClick={ this.props.clickDecrement }>减1</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Store from './ReduxDemo/Store';
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+const reducer = (state = 0, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+
+const render = () => {
+  ReactDOM.render(<Store
+    clickIncrement={ () => store.dispatch({ type: INCREMENT })}
+    clickDecrement={ () => store.dispatch({ type: DECREMENT })}
+    value={ store.getState() }
+  />, document.getElementById('root'));
+};
+render();
+store.subscribe(() => render())
 
